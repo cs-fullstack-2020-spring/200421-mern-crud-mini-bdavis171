e890a36a0f1612d4e9aa0199cdde5f0164b41261
bdavis171 <bdavis171@gmail.com>
2020-04-21 12:59:27 -0500

    connected database to client, finished AddCardForm component

---
+++ client/src/components/AddCardForm.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+
+
+class AddCardForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            name: "",
+            cardNumber: 0,
+            phone: 0,
+            zipCode: 0
+         }
+    }
+
+    // handle changes to fields
+    handleChanges = (event) => {
+        if (event.target.name === "name"){
+            this.setState({ name: event.target.value});
+        }
+        else if (event.target.name === "cardNumber"){
+            this.setState({ cardNumber: event.target.value});
+        }
+        else if (event.target.name === "phone"){
+            this.setState({ phone: event.target.value});
+        }
+        else if (event.target.name === "zipCode"){
+            this.setState({ zipCode: event.target.value});
+        }
+    }
+
+    // handle submission
+    handleSubmission = async(event) => {
+        event.preventDefault();
+        let formData = {
+            name: this.state.name,
+            cardNumber: this.state.cardNumber,
+            phone: this.state.phone,
+            zipCode: this.state.zipCode
+        };
+        const response = await fetch('/api' , {
+            method: "POST",
+            headers: {
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+        const json = await response.json();
+        console.log(json);
+    }
+
+    render() { 
+        return ( 
+            <form action="">
+                <fieldset>
+                    <legend>Add A Card</legend>
+                    <div className="form-group">
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" name='name' id='name' value={this.state.name} onChange={this.handleChanges}/>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="cardNumber">Card Number:</label>
+                        <input type="number" name='cardNumber' id='cardNumber' value={this.state.cardNumber} onChange={this.handleChanges}/>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="phone">Phone Number:</label>
+                        <input type="number" name='phone' id='phone' value={this.state.phone} onChange={this.handleChanges}/>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="zipCode">Zip Code:</label>
+                        <input type="number" name='zipCode' id='zipCode' value={this.state.zipCode} onChange={this.handleChanges}/>
+                    </div>
+
+                    <div className="form-group">
+                       <button onClick={this.handleSubmission}>Add Card</button>
+                    </div>
+                </fieldset>
+            </form>
+         );
+    }
+}
+ 
+export default AddCardForm;
---
+++ client/src/components/AppContainer.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import AddCardForm from './AddCardForm';
+import DisplayCard from './DisplayCard';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+
+class AppContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            willAdd: false,
+            cardList: []
+        }
+    }
+
+    componentDidMount = () => {
+        this.loadData();
+    }
+
+    // load data
+    loadData = async () => {
+        const response = await fetch('/api');
+        const json = await response.json();
+        console.table(json);
+        console.log('hello');
+        this.setState({ cardList: json });
+    }
+
+    // handle willAdd
+    handleWillAdd = (event) => {
+        if (this.state.willAdd) {
+            this.setState({ willAdd: false });
+        }
+        else {
+            this.setState({ willAdd: true });
+        }
+    }
+
+
+
+    render() {
+        let addForm;
+        if (this.state.willAdd) {
+            addForm = <AddCardForm />;
+        }
+
+        return (
+            <div className='home-container'>
+                <div className="header">
+                    <h1>Library Card Records</h1>
+                </div>
+
+                <div className="add-form">
+                    <button onClick={this.handleWillAdd}>Click here to add a new card!</button>
+                    {addForm}
+                </div>
+
+                <div className="card-list">
+                    <fieldset>
+                        <legend>List of Library Cards</legend>
+                        <Router>
+                        {this.state.cardList.map(
+                            (card) => {
+                               
+                                return (
+                                    <div key={card._id}>
+                                        
+                                            <Link to={`/api/${card.cardNumber}`}>
+                                                <p>Name: {card.name}</p>
+                                                <p>Card Number: {card.cardNumber}</p>
+                                            </Link>
+                                            <Route path={`/api/${card.cardNumber}`}>
+                                                <DisplayCard card={card}/>
+                                            </Route>
+                                       
+                                    </div>
+                                )
+                            }
+                        )}
+                        </Router>
+                    </fieldset>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default AppContainer;
